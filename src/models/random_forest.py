dc0e30e1347f456c260d1932bd50ac8cce856942
Raghav <rags2596@gmail.com>
2025-02-18 01:30:06 +1000

    Random Forest Classifier updated
    Tested and implemented Random Forest Classifier using Gini impurity criteria

--- src/models/random_forest.py
+++ src/models/random_forest.py
@@ -122,20 +122,28 @@ class DecisionTree:
         
 
 class RandomForest:
-    def __init__(self, n_trees=10, max_depth=None):
+    def __init__(self, n_trees=10, min_samples_split = 2, n_features = None, max_depth=100):
         self.n_trees = n_trees
         self.max_depth = max_depth
+        self.min_samples_split = min_samples_split
+        self.n_features =  n_features
         self.trees = []
 
     def fit(self, X, y):
         """Train multiple trees on bootstrapped datasets"""
+        self.trees = []
         for _ in range(self.n_trees):
-            indices = np.random.choice(len(X), len(X), replace=True)
-            X_sample, y_sample = X.loc[indices], y.loc[indices]
+            tree = DecisionTree(max_depth = self.max_depth, min_samples_split = self.min_samples_split, n_features = self.n_features)
 
-            tree = DecisionTree(self.max_depth)
+            X_sample, y_sample = self._bootstrap_samples(X, y)
             tree.fit(X_sample, y_sample)
             self.trees.append(tree)
+
+    def _bootstrap_samples(self, X, y):
+        n_samples = X.shape[0]
+        idxs = np.random.choice(n_samples, n_samples, replace = True)
+        return X[idxs], y[idxs]
+
 
     def predict(self, X):
         """Aggregate predictions from all trees (majority voting)"""
